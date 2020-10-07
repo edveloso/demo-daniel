e9670dfec66e970ad3a0f7110a078eb5a11a6cef
edveloso <edveloso@gmail.com>
2020-10-06 21:51:10 -0300

    commit inicial

---
+++ src/main/java/br/edu/infnet/App.java
@@ -0,0 +1,16 @@
+package br.edu.infnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    private String nome;
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );        
+    }
+}
---
+++ src/test/java/br/edu/infnet/AppTest.java
@@ -0,0 +1,21 @@
+package br.edu.infnet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        
+    }
+}
